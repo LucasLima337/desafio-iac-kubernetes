b70ea2fe349bbcf6b9d008440ed35ec036987cb1
Lucas Lima <luc.tech3337@gmail.com>
2022-06-10 18:59:27 -0300

    refactor: using variables

--- k8s.tf
+++ k8s.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_kubernetes_cluster" "k8s_iniciativa_devops" {
-  name    = "k8s-iniciativa-devops"
-  region  = "nyc1"
+  name    = var.cluster_name
+  region  = var.cluster_region
   version = "1.22.8-do.1"
 
   node_pool {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "cluster_name" {
+  type        = string
+  description = "Cluster Name"
+}
+
+variable "cluster_region" {
+  type        = string
+  description = "Cluster Region"
+}
+
 variable "digitalocean_api_token" {
   type        = string
   description = "DigitalOcean API token"
